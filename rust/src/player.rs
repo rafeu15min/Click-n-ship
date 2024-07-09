9ce1bddcebcddaba7da8583ab5426f16c263a55b
Rafael de Paulo <rafaeldepaulo7@hotmail.com>
2024-07-08 22:41:37 -0300

    Slippy Universe

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -9,9 +9,12 @@ use godot::prelude::*;
 pub struct Player {
     #[export]
     speed: f64,
-    target: Vector2,
     target_move: Vector2,
     base: Base<CharacterBody2D>,
+    acceleration: f32,
+    #[export]
+    max_acceleration: f32,
+    is_moving: bool
 }
 
 #[godot_api]
@@ -19,9 +22,11 @@ impl ICharacterBody2D for Player {
     fn init(base: Base<CharacterBody2D>) -> Self {
         Self {
             speed: 750.0,
-            target: Vector2::new(600.0, 300.0),
             target_move: Vector2::new(600.0, 300.0),
             base,
+            acceleration: 0.0,
+            max_acceleration: 1.5,
+            is_moving: false
         }
     }
 
@@ -29,8 +34,8 @@ impl ICharacterBody2D for Player {
         let event1 = event.clone();
         match event.try_cast::<InputEventMouseButton>() {
             Ok(e) => {
-                if e.get_button_index() == MouseButton::RIGHT && e.is_pressed() {
-                    self.target = self.base().get_global_mouse_position();
+                if e.get_button_index() == MouseButton::RIGHT {
+                    self.is_moving = e.is_pressed();
                 }
             }
             Err(_) => {}
@@ -44,15 +49,24 @@ impl ICharacterBody2D for Player {
     }
 
     fn physics_process(&mut self, _delta: f64) {
-        let target = self.target;
+        
         let target_move = self.target_move;
-        let velocity = self.base().get_position().direction_to(target).normalized() * self.speed as f32;
-        self.base_mut().set_velocity(velocity);
-        self.base_mut().look_at(target_move);
-        if self.base().get_position().distance_to(target) > 10.0 {
-            self.base_mut().move_and_slide();
-        }
-            
-    }
+        
 
+        if self.is_moving && self.acceleration <= self.max_acceleration{
+            self.acceleration += 0.02;
+        }
+        if !self.is_moving {
+            self.acceleration -= 0.01;
+        }
+        if self.acceleration < 0.0 || self.base().get_position().distance_to(target_move) < 10.0{
+            self.acceleration = 0.0;
+        }
+        
+        self.base_mut().look_at(target_move);
+        let velocity = self.base().get_transform().a * self.speed as f32 * self.acceleration;
+        self.base_mut().set_velocity(velocity);
+
+        self.base_mut().move_and_slide();
+    }
 }
